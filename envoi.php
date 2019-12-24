54ec6550245c8ad6c856ba4aa71ae9ad1b9edf75
Zoro-Bi <zorobizah@yahoo.fr>
2019-12-24 05:07:19 +0100

    modif

--- envoi.php
+++ envoi.php
@@ -42,12 +42,14 @@
 
     $folder = "cache/";
 
+    echo basename( $_FILES['file']['name'])."\n";
+
     $path = $folder . basename( $_FILES['file']['name']); 
 
     if(move_uploaded_file($_FILES['file']['tmp_name'], $path)) {
         echo "The file ".  basename( $_FILES['file']['name']). " has been uploaded";
     } else{
-    echo "There was an error uploading the file, please try again!";
+    echo "There was an error uploading the file, please try again! \n";
     }
 
     $path = $_SERVER['DOCUMENT_ROOT'];
