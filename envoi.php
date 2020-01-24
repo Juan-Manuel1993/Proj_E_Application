76e55d184b426c0ff0befdd4fdbde1f965b1c1e8
SullyvanT <tremor.sullyvan@gmail.com>
2020-01-24 13:39:31 +0100

    Add regex

--- envoi.php
+++ envoi.php
@@ -1,249 +1,7 @@
 <?php
 
     session_start();
-    // afficher incrémentalement
-    // sauvegarder les données en cache
-    // gérer la durée de vie du cache
-    // si les donnnées sont déjà en cache pas la peine de chercher sur le serveur (ce qui dimuniera la durée de récupération des données)
-    // récupérer les poids les plus forts
-    // faire une api qui récupère le type de requête du client
-    // id triées par ordre de poids décroissants
-    // tri par ordre alphabétique (plus dure) avec les poids. Prendre les n plus forts et les afficher par ordre alphabétique
-    // gestion de la mémoire (côté client)
-    // On peut charger tous les mots communs en avance (animal etc)
-    // on doit pouvoir masquer des relations
-    // garder ce qu'on a déjà demander au serveur pour ne pas l'afficher deux fois
-    //Gestion des relations négatives
-    //Gestion de l'affichage
 
-    /*
-    display_errors = On
-    display_startup_errors = On
-    error_reporting = E_ALL
-    html_errors = On
-
-    xdebug.default_enable = 1
-    xdebug.max_nesting_level = 200
-    */
-
-/*	$i=$_POST['mot']; // Récupère ce que l'utilisateur a entré
-    $result = getdata($i);
-
-    //$file = "noeuds$i.txt";
-
-    $noeuds = "./noeuds$i.txt"; // fichier qui contient tous les noeuds
-    $rs ="./rs$i.txt"; // fichier qui contient toutes les relations sortantes
-    $re ="./re$i.txt"; // fichier qui contient toutes les relations entrantes
-    $ty ="./ty$i.txt"; // fichier qui contient les types de relations
-
-    //iconv(mb_detect_encoding($result, mb_detect_order(), true), "UTF-8", $result);
-
-    //$result = utf8_encode($result);
-    //$result = html_entity_decode($result,ENT_QUOTES,"UTF-8");
-
-    $definitionmots = getdef($result);
-
-    // Découper le contenu de la page par <code>
-    $decoupeparcode = explode('<CODE>',$result);
-
-
-    $apresformatedname = explode('formated name\'',$decoupeparcode[1]); // récupérer tout ce qui est après le mot et avant  " formated name " dans le texte. Ce qui correspond à tout ce dont on a besoin à partir de noeud
-
-    // Découper apresformatedname[1] par //" tous les noeuds sont dans $getnoeud[0]
-    $getnoeud = explode('//',$apresformatedname[1]);
-
-    //toutes les types de relations sont dans $gettyperelation[1]
-    $gettyperelation = explode('rthelp\'',$getnoeud[1]);
-
-    // recupère tout le texte apres "formated name"
-    //contient toutes les relations
-    $touterelation = explode('les relations sortantes : r;rid;node1;node2;type;w ',$apresformatedname[1]);
-
-    // découpe le texte par // pour récupérer les relations
-    $getrelationsortante = explode('//',$touterelation[1]);
-    //$getrelationsortante[0] contient toutes les relations sortantes
-
-    // $getrelationentrante[1] contient toutes les relations entrantes
-    $getrelationentrante = explode('les relations entrantes : r;rid;node1;node2;type;w ',$getrelationsortante[1]);
-
-    // enlever le r; du début des relations sortantes
-    //$getrelationsortante2 nouvelle table de relation sortante
-
-    $getrelationsortante2 = explode('r;', $getrelationsortante[0]);
-
-    //récupère la taille du tableau de relation sortante
-    //$taillerelationsortante = $count($getrelationsortante2);
-
-    // enlève le e; du début des noeuds
-    // getnoeuds nouveau tableau de noeuds
-    $getnoeuds = explode('e;', $getnoeud[0]);
-
-    // enleve "rt;" du debut de tous les types de relation
-    // suprimert devient notre nouveau contenu de type de relation
-
-    $supprimert = explode('rt;', $gettyperelation[1]);
-
-    //on récupère le nom de chaque type de relations existant
-    for ($i=1; $i<count($supprimert);$i++){
-
-        $r = explode(';', $supprimert[$i]);
-
-        $type[$r[0]] = $r[1];
-
-    }
-
-    for ($i=1; $i<count($getrelationsortante2);$i++){
-
-        //	decoupe chaque ligne de la table des relations sortantes par un ";"
-        $ri = explode(';', $getrelationsortante2[$i]);
-
-        // crée une table de hachache de numero de relation sortante =====> poids
-        $relationsortante[$ri[0]] = $ri[4];
-
-        // crée une table de hachache de numero de relation sortante =====> identifiant du noeud
-        $relationsortante2[$ri[0]] = $ri[2];
-
-        // crée une table de hachache de numero de relation sortante =====> type de relation
-        $typerelation[$ri[0]] = $ri[3];
-
-    }
-
-    for ($i=2; $i<count($getnoeuds);$i++){
-
-        // decoupe chaque ligne de la table des noeuds par un ";"
-        $b = explode(';', $getnoeuds[$i]);
-
-        //contient chaque noeud et son type
-        if ($b[2] == 1){
-            $typen[$b[0]] = "n_term";
-        }
-
-        elseif ($b[2] == 2) {
-            $typen[$b[0]] = "n_form";
-        }
-
-        elseif ($b[2] == 4){
-            $typen[$b[0]] = "n_pos";
-        }
-
-        elseif ($b[2] == 6){
-            $typen[$b[0]] = "n_flpot";
-        }
-
-        elseif ($b[2] == 8){
-            $typen[$b[0]] = "n_chunk";
-        }
-
-        elseif ($b[2] == 9){
-            $typen[$b[0]] = "n_question";
-        }
-
-        elseif ($b[2] == 10) {
-            $typen[$b[0]] = "n_relation";
-
-        }
-        elseif ($b[2] == 12) {
-            $typen[$b[0]] = "n_analogy";
-        }
-
-        elseif ($b[2] == 18){
-            $typen[$b[0]] = "n_data";
-        }
-
-        elseif ($b[2] == 36){
-            $typen[$b[0]] = "n_data_pot";
-        }
-
-        elseif ($b[2] == 200) {
-            $typen[$b[0]] = "n_context";
-        }
-
-        elseif ($b[2] == 444) {
-            $typen[$b[0]] = "n_link";
-        }
-
-        elseif ($b[2] == 666){
-            $typen[$b[0]] = "n_AKI";
-        }
-
-        elseif ($b[2] == 777) {
-            $typen[$b[0]] = "n_wikipedia";
-        }
-
-        elseif ($b[2] == 1002){
-            $typen[$b[0]] = "n_group";
-        }
-
-
-
-
-
-        // si c'est un string je récupère le string sans les identifiants (nettoyage)
-        if (is_string($b[4])) {
-            $node[$b[0]] = $b[4];
-        }else{
-            // recupère nom du noeud
-            $node[$b[0]] = $b[1];
-        }
-
-        //$node contient le nom des noeuds
-
-    }
-
-    #je supprime de la table des noeuds, tous les noeuds qui ne sont pas dans la table de relation sortante
-    foreach ($node as $key => $value) {
-        if (!in_array($key, $relationsortante2)) {
-            unset($node[$key]);
-        }
-    }
-
-    #création d'un tableau relation(identifiant) => nom de noeud
-    foreach ($relationsortante2 as $key => $value) {
-        $relationsortante2[$key] = $node[$value];
-        $relationsortante3[$key] = $typen[$value];
-    }
-
-    //print_r($relationsortante3);
-
-    foreach ($typerelation as $key => $value) {
-        // création d'un tableau de noeuds liés par la relation r_raff_sem au noeud cherché
-        if($typerelation[$key] == 1){
-            $po = explode('\'', $relationsortante2[$key]);
-            $raff[] = $po[1];
-        }
-    }
-
-    //print_r($raff);
-
-    foreach ($typerelation as $key => $value) {
-        $typerelation[$key] = $type[$value];
-    }
-
-    //print_r($typerelation);
-    ksort($typerelation);
-
-    //print_r($typerelation);
-
-    #tri par clé croissant
-    ksort($relationsortante3);
-
-    ksort($typen);
-
-    #tri par valeur croissant
-    ksort($relationsortante2);
-
-    ksort($relationsortante);
-
-    foreach ($relationsortante2 as $key => $value) {
-        $relationsortante2[$key] =  $relationsortante[$key].",".$relationsortante2[$key].",".$typerelation[$key].",".$relationsortante3[$key].";";
-    }
-
-    //print_r($relationsortante2);
-
-    $toutdef = array_map('raffinement', $raff);
-    //print_r($toutdef);
-
-*/
     function getdata($arg)
     {
         $postfields = http_build_query(array(
@@ -269,11 +27,25 @@
         return $result;
     }
 
-    
+    function getToken($text, $regexAll, $regexSingle)
+    {
+        $tokens = array();
+
+        preg_match_all($regexAll, $text, $tab);
+
+        foreach ($tab[1] as $key => $value) {
+            preg_match($regexSingle, $value, $token);
+            $tokens[] = $token;
+        }
+
+        return $tokens;
+    }
+
     function getmots()
     {
-    
         $url = "http://www.jeuxdemots.org/JDM-LEXICALNET-FR/01012020-LEXICALNET-JEUXDEMOTS-ENTRIES.txt";
+        $i =0;
+        $nbMaxValue = 1000;
 
         $options = array(
             'http' => array(
@@ -287,12 +59,30 @@
 
         $result = file_get_contents($url, false, $context);
         $result = utf8_encode($result);
-      
 
-        return $result;
+        $regexAll = '/([\n]*\d+;[^\n]+;)/u';
+        $regexSingle = '/\d+;(?P<name>[^\n]+);/u';
+
+        $tokens = array();
+
+        preg_match_all($regexAll, $text, $tab);
+
+        foreach ($tab[1] as $key => $value) {
+            preg_match($regexSingle, $value, $token);
+            $tokens[] = $token;
+
+            $i++;
+            if ($i > $nbMaxValue) {
+                break;
+            }
+        }
+
+        return $tokens;
+
+        print_r($tokens);
+
+        return $tokens;
     }
-
-
 
     function getdef($arg)
     {
@@ -608,20 +398,7 @@
 
     ///////////////
 
-    function getToken($text, $regexAll, $regexSingle)
-    {
-        $tokens = array();
 
-        preg_match_all($regexAll, $text, $tab);
-
-
-        foreach ($tab[1] as $key => $value) {
-            preg_match($regexSingle, $value, $token);
-            $tokens[] = $token;
-        }
-
-        return $tokens;
-    }
 
     function getEntries($data)
     {
@@ -705,11 +482,3 @@
 
         return $new_array;
     }
-
-        ///////////////
-                /*
-        $word = 'chat';
-        echo 'result :';
-        print_r(getInformations($word, getdata($word)));
-*/
-                ////////////////
--- index.php
+++ index.php
@@ -1,79 +1,63 @@
 <?php
-    require('envoi.php');
-    include 'init.php';
+require('envoi.php');
+include 'init.php';
 
-    if (empty(session_id())) {
-        session_start();
-    }
+if (empty(session_id())) {
+    session_start();
+}
 
-    class Word
+class Word
+{
+    public $weight;
+    private $word;
+
+    public function __construct($weight, $word)
     {
-        public $weight;
-        private $word;
-
-        public function __construct($weight, $word)
-        {
-            $this->setWeight($weight);
-            $this->setWord($word);
-        }
-
-        public function getWeight()
-        {
-            return $this->weight;
-        }
-
-        public function setWeight($weight)
-        {
-            $this->weight = $weight;
-        }
-
-        public function getWord()
-        {
-            return $this->word;
-        }
-
-        public function setWord($word)
-        {
-            $this->word = $word;
-        }
+        $this->setWeight($weight);
+        $this->setWord($word);
     }
 
-    $jdm_result = array();
-
-    $data = "";
-    $word = "";
-
-    if (isset($_POST['mot'])) {
-        $_SESSION['mot'] = $_POST['mot'];
-        $data = getdata($_POST['mot']);
-        $word = $_POST['mot'];
-    } elseif (isset($_SESSION['mot'])) {
-        $word = $_SESSION['mot'];
-        $data = getdata($word);
-    } else {
-        $word = 'chat';
-        $data = getdata($word);
+    public function getWeight()
+    {
+        return $this->weight;
     }
 
-    $info = getInformations($word, $data);
-    $tout = getmots();
-
-   
-   // $var= explode('id;terme;', $tout);
-
-    $var = explode("\n", $tout);
-
-    
-   /* $varr = explode("\n", $var[);
-    // pas 4->5, 7->8,1->11
-
-    */
-    for ($i=1200000; $i <1201000; $i++) { 
-      $t = explode(';', $var[$i]);
-      $contenu2[]=$t[1];
+    public function setWeight($weight)
+    {
+        $this->weight = $weight;
     }
 
-   
+    public function getWord()
+    {
+        return $this->word;
+    }
+
+    public function setWord($word)
+    {
+        $this->word = $word;
+    }
+}
+
+$jdm_result = array();
+
+$data = "";
+$word = "";
+
+if (isset($_POST['mot'])) {
+    $_SESSION['mot'] = $_POST['mot'];
+    $data = getdata($_POST['mot']);
+    $word = $_POST['mot'];
+} elseif (isset($_SESSION['mot'])) {
+    $word = $_SESSION['mot'];
+    $data = getdata($word);
+} else {
+    $word = 'chat';
+    $data = getdata($word);
+    $_SESSION['mot'] ='chat';
+}
+
+$info = getInformations($word, $data);
+getmots();
 
 ?>
 <!DOCTYPE html>
@@ -83,9 +67,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script type="text/javascript" src="tri.js"></script>
   <script scr="https://code.jquery.com/jquery-3.4.1.min.js"></script>
@@ -93,197 +75,206 @@
 
   <style>
 
-    div { border: 1px solid black;margin:15px }
-    table { solid #000;padding: 5px; }
-    td {solid #000; padding: 5px;}
-    th {padding: 5px;}
-    #conteneur { overflow: hidden}
-    #conteneur div { margin:5px;width:49.2%;float:left }
-    .whiteBackground { background-color: #fff; }
-	.grayBackground { background-color: #ccc; }
+  div { border: 1px solid black;margin:15px }
+  table { solid #000;padding: 5px; }
+  td {solid #000; padding: 5px;}
+  th {padding: 5px;}
+  #conteneur { overflow: hidden}
+  #conteneur div { margin:5px;width:49.2%;float:left }
+  .whiteBackground { background-color: #fff; }
+  .grayBackground { background-color: #ccc; }
 
-	/* Classe obligatoire pour les flèches */
-	.flecheDesc {
-	  width: 0;
-	  height: 0;
-	  float:right;
-	  margin: 10px;
-	  border-left: 5px solid transparent;
-	  border-right: 5px solid transparent;
-	  border-bottom: 5px solid black;
-	}
-	.flecheAsc {
-	  width: 0;
-	  height: 0;
-	  float:right;
-	  margin: 10px;
-	  border-left: 5px solid transparent;
-	  border-right: 5px solid transparent;
-	  border-top: 5px solid black;
-	}
+  /* Classe obligatoire pour les flèches */
+  .flecheDesc {
+    width: 0;
+    height: 0;
+    float:right;
+    margin: 10px;
+    border-left: 5px solid transparent;
+    border-right: 5px solid transparent;
+    border-bottom: 5px solid black;
+  }
 
-	/* Classe optionnelle pour le style */
-	.tableau {width:100%;table-layout: fixed;border-collapse: collapse;}
-	.tableau td {padding:.3rem}
-	.avectri th {text-align:center;padding:5px 0 0 5px;vertical-align: middle;background-color:#999690;color:#444;cursor:pointer;
-		-webkit-touch-callout: none;
-	  -webkit-user-select: none;
-	  -khtml-user-select: none;
-	  -moz-user-select: none;
-	  -ms-user-select: none;
-	  -o-user-select: none;
-	  user-select: none;
-	}
-	.avectri th.selection {background-color:#5d625c;color:#fff;}
-	.avectri th.selection .flecheDesc {border-bottom-color: white;}
-	.avectri th.selection .flecheAsc {border-top-color: white;}
+  .flecheAsc {
+    width: 0;
+    height: 0;
+    float:right;
+    margin: 10px;
+    border-left: 5px solid transparent;
+    border-right: 5px solid transparent;
+    border-top: 5px solid black;
+  }
 
-	.avectri tr:nth-child(odd) {background-color: #d6d3ce;border-bottom:1px solid #ccc;color:#444;}
-	.avectri tr:nth-child(even) {background-color: #c6c3bd;border-bottom:1px solid #ccc;color:#444;}
-	.avectri tbody tr:hover:nth-child(odd) {background-color: #999690;color:#ffffff;}
-	.avectri tbody tr:hover:nth-child(even) {background-color: #999690;color:#ffffff;}
+  /* Classe optionnelle pour le style */
+  .tableau {width:100%;table-layout: fixed;border-collapse: collapse;}
+  .tableau td {padding:.3rem}
+  .avectri th {text-align:center;padding:5px 0 0 5px;vertical-align: middle;background-color:#999690;color:#444;cursor:pointer;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -o-user-select: none;
+    user-select: none;
+  }
+  .avectri th.selection {background-color:#5d625c;color:#fff;}
+  .avectri th.selection .flecheDesc {border-bottom-color: white;}
+  .avectri th.selection .flecheAsc {border-top-color: white;}
 
+  .avectri tr:nth-child(odd) {background-color: #d6d3ce;border-bottom:1px solid #ccc;color:#444;}
+  .avectri tr:nth-child(even) {background-color: #c6c3bd;border-bottom:1px solid #ccc;color:#444;}
+  .avectri tbody tr:hover:nth-child(odd) {background-color: #999690;color:#ffffff;}
+  .avectri tbody tr:hover:nth-child(even) {background-color: #999690;color:#ffffff;}
 }
-	.zebre tbody td:nth-child(3) {text-align:center;}
+.zebre tbody td:nth-child(3) {text-align:center;}
+.preload {
+  margin:0;
+  position:absolute;
+  top:50%;
+  left:50%;
+  margin-right: -50%;
+  transform:translate(-50%, -50%);
+  border:0;
+}
+.postload {
+  display:none;
+}
+</style>
 
-  </style>
-  </head>
-  <body>
+<script>
+$( document ).ready(function() {
+  jQuery("tr td" ).click( function () {
+    $.post("index.php",
+    {
+      mot: $(this).text(),
+    },
+    function(data,status){
 
-    <h1 align="center">Jeux De Mots</h1>
-    <form method="post">
-
-      <p align="center">
-
-        <input type="text" id="mot" name="mot" value=<?php echo $_SESSION['mot']; ?> />
-        <input type="submit" id="formsubmit" value="Rechercher">
-
-      </p>
-    </form>
-
-
-     <script type="text/javascript"  charset="utf-8">
-
-      //var disp = <?php echo '["' . implode('", "', $contenu) . '"]' ?>;
-
-
-      var disp2 = <?php echo '["' . implode('", "', $contenu2) . '"]' ?>;
-
-
-       
-    
-    
-/*
-  $( function() {
-    
-    $( "#tags" ).autocomplete({
-      source: disp
     });
-  } );
- 
-*/
 
-  $( function() {
-    
-    $( "#mot" ).autocomplete({
-      source: disp2,
-      minLength:3
-    });
-  } );
+    document.location.reload(false);
 
- 
-  </script>
+    $(".preload").removeClass("postload");
+    $("#conteneur").addClass("postload");
 
+  });
+});
 
-    <div id="conteneur">
+$(document).load(function () {
+  $(".preload").addClass("postload");
+  $("#conteneur").removeClass("postload");
+});
+
+</script>
+</head>
+<body>
+
+  <h1 align="center">Jeux De Mots</h1>
+
+  <div class="preload postload">
+    <img src="loading.gif" />
+  </div>
+
+  <form method="post">
+
+    <p align="center">
+
+      <input type="text" id="mot" name="mot" value=<?php echo $_SESSION['mot']; ?> />
+      <input type="submit" id="formsubmit" value="Rechercher">
+
+    </p>
+  </form>
+
+  <div id="conteneur">
+    <div >
       <div >
-        <div >
-        	<form  action="envoi.php" method="post">
-            <p>Nombre d'élèment à afficher
+
+        <form  method="post">
+          <p>Nombre d'élèment à afficher
             <br>
-              <select name="mNbAffichage" id="mNbAffichage">
-                <?php
-                    for ($i = $init_min_tuples;$i <= 500; $i+=$init_step) {
-                        if ($i==$_POST['mNbAffichage']) {
-                            echo "<option selected=".'"'."selected".'"'." value=".$i.">".$i."</option>";
-                        } else {
-                            echo "<option value=".$i.">".$i."</option>";
-                        }
-                    }
-                ?>
-              </select>
-            </p>
-            <input type="submit" value="Appliquer" />
-          </form>
-        </div>
-        <table border="1" cellpadding="10" class="avectri" cellspacing="2" width="100%" >
-        	<thead>
+            <select name="mNbAffichage" id="mNbAffichage">
+              <?php
+              for ($i = $init_min_tuples;$i <= 500; $i+=$init_step) {
+                  if ($i==$_POST['mNbAffichage']) {
+                      echo "<option selected=".'"'."selected".'"'." value=".$i.">".$i."</option>";
+                  } else {
+                      echo "<option value=".$i.">".$i."</option>";
+                  }
+              }
+              ?>
+            </select>
+          </p>
+          <input type="submit" value="Appliquer" />
+        </form>
+      </div>
+      <table border="1" cellpadding="10" class="avectri" cellspacing="2" width="100%" >
+        <thead>
           <tr>
             <th data-tri="0" class="selection" style="width: min-content" data-type="num" width="10%">Importance</th>
             <th style="width: min-content">Mot</th>
           </tr>
-          </thead>
-          <tbody>
+        </thead>
+        <tbody>
           <?php
 
-            if (isset($_POST['mNbAffichage'])) {
-                $selected_val = $_POST['mNbAffichage'];
-            } else {
-                $selected_val = $init_min_tuples;
-            }
+          if (isset($_POST['mNbAffichage'])) {
+              $selected_val = $_POST['mNbAffichage'];
+          } else {
+              $selected_val = $init_min_tuples;
+          }
 
-              if ($data !== null) {
-                  $entries = getEntries($data);
+          if ($data !== null) {
+              $entries = getEntries($data);
 
-                  $i=0;
-                  foreach ($entries as $key => $tab) {
-                      $jdm_result[] = new Word(
-                          $tab['w'],
-                          str_replace("'", "", $tab['name'])
-                      );
-                      $i++;
+              $i=0;
+              foreach ($entries as $key => $tab) {
+                  $jdm_result[] = new Word(
+                      $tab['w'],
+                      str_replace("'", "", $tab['name'])
+                  );
+                  $i++;
 
-                      if ($i >= $selected_val) {
-                          break;
-                      }
+                  if ($i >= $selected_val) {
+                      break;
                   }
               }
+          }
 
-            for ($i = 0;$i < $selected_val;$i++) {
-                if ($i%2 == 1) {
-                    $color_bg = '#fff';
-                } else {
-                    $color_bg = '#ccc';
-                }
+          for ($i = 0;$i < $selected_val;$i++) {
+              if ($i%2 == 1) {
+                  $color_bg = '#fff';
+              } else {
+                  $color_bg = '#ccc';
+              }
 
-                if ($jdm_result[$i] != null) {
-                    echo "
-								<tr bgcolor=".$color_bg.">
-								<td>".$jdm_result[$i]->getWeight()."</td>
-								<td>".$jdm_result[$i]->getWord()."</td>
-								</tr>
-							";
-                }
-            }
+              if ($jdm_result[$i] != null) {
+                  echo "
+              <tr  bgcolor=".$color_bg.">
+              <td>".$jdm_result[$i]->getWeight()."</td>
+              <td class=\"fund\">".$jdm_result[$i]->getWord()."</td>
+              </tr>
+              ";
+              }
+          }
           ?>
-          </tbody>
-        </table>
-      </div>
 
-      <div>
-        <h2>Mot de la recherche : <h1><?php echo $word; ?></h1> </h2>
-          <h3>Définition : </h3><br>
-          <?php echo $info['def'] ?>
-          <?php foreach ($info['raffSem'] as $key => $tab) {
+        </tbody>
+      </table>
+    </div>
+
+    <div>
+      <h2>Mot de la recherche : <h1><?php echo $word; ?></h1> </h2>
+      <h3>Définition : </h3><br>
+      <?php echo $info['def'] ?>
+      <?php foreach ($info['raffSem'] as $key => $tab) {
               if ($tab['def'] != "") {
                   echo $tab['def'];
               }
           }
-          ?>
-      </div>
+      ?>
     </div>
+  </div>
 
 
-
-  </body>
+</body>
 </html>
