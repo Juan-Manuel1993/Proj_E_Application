954a082336006dbfd3ea60701a813a21eb64eba2
SullyvanT <tremor.sullyvan@gmail.com>
2020-01-25 23:54:56 +0100

    Remove  old files

--- index.php
+++ index.php
@@ -8,11 +8,13 @@ if (empty(session_id())) {
 
 class Word
 {
-    public $weight;
+    private $weight;
     private $word;
+    private $eid=0;
 
-    public function __construct($weight, $word)
+    public function __construct($eid, $weight, $word)
     {
+        $this->setId($eid);
         $this->setWeight($weight);
         $this->setWord($word);
     }
@@ -36,6 +38,16 @@ class Word
     {
         $this->word = $word;
     }
+
+    public function getId()
+    {
+        return $this->eid;
+    }
+
+    public function setId($eid)
+    {
+        $this->eid = $eid;
+    }
 }
 
 $jdm_result = array();
@@ -56,224 +68,213 @@ if (isset($_POST['mot'])) {
     $_SESSION['mot'] ='chat';
 }
 
-$info = getInformations($word, $data);
+$news = getWordInfo($word, $data);
 
 ?>
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
+
 <head>
-  <title>Jeux Mots</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="tri.js"></script>
-  <script scr="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-  <script type="text/javascript" scr="mscript.js"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>WordPlay</title>
+  <!-- MDB icon -->
+  <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <!-- Google Fonts Roboto -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!-- Material Design Bootstrap -->
+  <link rel="stylesheet" href="css/mdb.min.css">
+  <!-- Your custom styles (optional) -->
+  <link rel="stylesheet" href="css/style.css">
+  <!-- MDBootstrap Datatables  -->
+  <link href="css/addons/datatables.min.css" rel="stylesheet">
 
   <style>
-
-  div { border: 1px solid black;margin:15px }
-  table { solid #000;padding: 5px; }
-  td {solid #000; padding: 5px;}
-  th {padding: 5px;}
-  #conteneur { overflow: hidden}
-  #conteneur div { margin:5px;width:49.2%;float:left }
-  .whiteBackground { background-color: #fff; }
-  .grayBackground { background-color: #ccc; }
-
-  /* Classe obligatoire pour les flèches */
-  .flecheDesc {
-    width: 0;
-    height: 0;
-    float:right;
-    margin: 10px;
-    border-left: 5px solid transparent;
-    border-right: 5px solid transparent;
-    border-bottom: 5px solid black;
+  .loader {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #ffffff;
   }
+  </style>
 
-  .flecheAsc {
-    width: 0;
-    height: 0;
-    float:right;
-    margin: 10px;
-    border-left: 5px solid transparent;
-    border-right: 5px solid transparent;
-    border-top: 5px solid black;
-  }
+</head>
 
-  /* Classe optionnelle pour le style */
-  .tableau {width:100%;table-layout: fixed;border-collapse: collapse;}
-  .tableau td {padding:.3rem}
-  .avectri th {text-align:center;padding:5px 0 0 5px;vertical-align: middle;background-color:#999690;color:#444;cursor:pointer;
-    -webkit-touch-callout: none;
-    -webkit-user-select: none;
-    -khtml-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    -o-user-select: none;
-    user-select: none;
-  }
-  .avectri th.selection {background-color:#5d625c;color:#fff;}
-  .avectri th.selection .flecheDesc {border-bottom-color: white;}
-  .avectri th.selection .flecheAsc {border-top-color: white;}
+<body>
 
-  .avectri tr:nth-child(odd) {background-color: #d6d3ce;border-bottom:1px solid #ccc;color:#444;}
-  .avectri tr:nth-child(even) {background-color: #c6c3bd;border-bottom:1px solid #ccc;color:#444;}
-  .avectri tbody tr:hover:nth-child(odd) {background-color: #999690;color:#ffffff;}
-  .avectri tbody tr:hover:nth-child(even) {background-color: #999690;color:#ffffff;}
-}
-.zebre tbody td:nth-child(3) {text-align:center;}
-.preload {
-  margin:0;
-  position:absolute;
-  top:50%;
-  left:50%;
-  margin-right: -50%;
-  transform:translate(-50%, -50%);
-  border:0;
-}
-.postload {
-  display:none;
-}
-</style>
+  <!-- jQuery -->
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <!-- Your custom scripts (optional) -->
+  <script type="text/javascript"></script>
+  <!-- MDBootstrap Datatables  -->
+  <script type="text/javascript" src="js/addons/datatables.min.js"></script>
 
-<script>
-$( document ).ready(function() {
-  jQuery("tr td" ).click( function () {
+  <script>
+
+  $(window).on("load",function(){
+    $(".loader").fadeOut("fast");
+  });
+
+  function search(row) {
+
     $.post("index.php",
     {
-      mot: $(this).text(),
+      mot: row.getElementsByTagName("td")[1].innerHTML,
     },
     function(data,status){
 
     });
 
     document.location.reload(false);
+  };
 
-    $(".preload").removeClass("postload");
-    $("#conteneur").addClass("postload");
+  $(document).ready(function() {
+    $('div table').DataTable({
+      "order": [[ 0, "desc" ]]
+    });
+    $('.dataTables_length').addClass('bs-select');
 
   });
-});
 
-$(document).load(function () {
-  $(".preload").addClass("postload");
-  $("#conteneur").removeClass("postload");
-});
 
 </script>
-</head>
-<body>
 
-  <h1 align="center">Jeux De Mots</h1>
+<h1 align="center">Jeux De Mots</h1>
 
-  <div class="preload postload">
-    <img src="loading.gif" />
+<div class="loader">
+  <img src="loading.gif" />
+</div>
+
+<form method="post">
+  <p align="center">
+    <input class="form-control form-control-sm mr-3 w-25" type="text" id="mot" name="mot" value=<?php echo $_SESSION['mot']; ?> />
+  </p>
+</form>
+
+<div id="conteneur">
+  <div>
+    <h2>Mot de la recherche : <h1><?php echo $word; ?></h1> </h2>
+    <h3>Définition : </h3><br>
+    <?php echo $news['definition'] ?>
+    <?php foreach ($news['raffinement'] as $key => $tab) {
+    if ($tab['def'] != "") {
+        echo $tab['def'];
+    }
+}
+    ?>
   </div>
 
-  <form method="post">
-
-    <p align="center">
-
-      <input type="text" id="mot" name="mot" value=<?php echo $_SESSION['mot']; ?> />
-      <input type="submit" id="formsubmit" value="Rechercher">
-
-    </p>
-  </form>
-
-  <div id="conteneur">
-    <div >
-      <div >
-
-        <form  method="post">
-          <p>Nombre d'élèment à afficher
-            <br>
-            <select name="mNbAffichage" id="mNbAffichage">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <table class="table table-hover table-striped table-bordered table-sm" cellspacing="0">
+          <thead>
+            <tr>
+              <th class="th-sm">Importance
+              </th>
+              <th class="w-auto">Mot sortant
+              </tr>
+            </thead>
+            <tbody>
               <?php
-              for ($i = $init_min_tuples;$i <= 500; $i+=$init_step) {
-                  if ($i==$_POST['mNbAffichage']) {
-                      echo "<option selected=".'"'."selected".'"'." value=".$i.">".$i."</option>";
-                  } else {
-                      echo "<option value=".$i.">".$i."</option>";
+              if ($data !== null) {
+                  $rsortantes = $news['RSortantes'];
+
+                  foreach ($rsortantes as $key => $tab) {
+                      $word = new Word(
+                          $tab['node'],
+                          getEntrie($news['entries'], $tab['node'])['w'],
+                          getEntrie($news['entries'], $tab['node'])['name']
+                      );
+
+                      if (!array_key_exists(getEntrie($news['entries'], $tab['node'])['eid'], $jdm_result)) {
+                          echo "
+                    <tr id=".$word->getId()." onclick=\"search(this)\">
+                    <td>".$word->getWeight()."</td>
+                    <td>".$word->getWord()."</td>
+                    </tr>
+                    ";
+                          $jdm_result[getEntrie($news['entries'], $tab['node'])['eid']] = $word;
+                      }
                   }
               }
               ?>
-            </select>
-          </p>
-          <input type="submit" value="Appliquer" />
-        </form>
-      </div>
-      <table border="1" cellpadding="10" class="avectri" cellspacing="2" width="100%" >
-        <thead>
-          <tr>
-            <th data-tri="0" class="selection" style="width: min-content" data-type="num" width="10%">Importance</th>
-            <th style="width: min-content">Mot</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php
-
-          if (isset($_POST['mNbAffichage'])) {
-              $selected_val = $_POST['mNbAffichage'];
-          } else {
-              $selected_val = $init_min_tuples;
-          }
-
-          if ($data !== null) {
-              $entries = getEntries($data);
-
-              $i=0;
-              foreach ($entries as $key => $tab) {
-                  $jdm_result[] = new Word(
-                      $tab['w'],
-                      str_replace("'", "", $tab['name'])
-                  );
-                  $i++;
-
-                  if ($i >= $selected_val) {
-                      break;
-                  }
-              }
-          }
-
-          for ($i = 0;$i < $selected_val;$i++) {
-              if ($i%2 == 1) {
-                  $color_bg = '#fff';
-              } else {
-                  $color_bg = '#ccc';
-              }
-
-              if ($jdm_result[$i] != null) {
-                  echo "
-              <tr  bgcolor=".$color_bg.">
-              <td>".$jdm_result[$i]->getWeight()."</td>
-              <td class=\"fund\">".$jdm_result[$i]->getWord()."</td>
+            </tbody>
+            <tfoot>
+              <tr>
+                <th>Importance
+                </th>
+                <th>Mot sortant
+                </th>
               </tr>
-              ";
-              }
-          }
-          ?>
+            </tfoot>
+          </table>
+        </div>
+        <div class="col">
+          <table class="table table-hover table-striped table-bordered table-sm" cellspacing="0">
+            <thead>
+              <tr>
+                <th class="th-sm">Importance
+                </th>
+                <th class="w-auto">Mot entrant
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                if ($data !== null) {
+                    $rentrantes = $news['REntrantes'];
 
-        </tbody>
-      </table>
+                    foreach ($rentrantes as $key => $tab) {
+                        $word = new Word(
+                            $tab['node'],
+                            getEntrie($news['entries'], $tab['node'])['w'],
+                            getEntrie($news['entries'], $tab['node'])['name']
+                        );
+
+                        if (!array_key_exists(getEntrie($news['entries'], $tab['node'])['eid'], $jdm_result)) {
+                            echo "
+                      <tr id=".$word->getId()." onclick=\"search(this)\">
+                      <td>".$word->getWeight()."</td>
+                      <td>".$word->getWord()."</td>
+                      </tr>
+                      ";
+                            $jdm_result[getEntrie($news['entries'], $tab['node'])['eid']] = $word;
+                        }
+                    }
+                }
+                ?>
+              </tbody>
+              <tfoot>
+                <tr>
+                  <th>Importance
+                  </th>
+                  <th>Mot entrant
+                  </th>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
 
-    <div>
-      <h2>Mot de la recherche : <h1><?php echo $word; ?></h1> </h2>
-      <h3>Définition : </h3><br>
-      <?php echo $info['def'] ?>
-      <?php foreach ($info['raffSem'] as $key => $tab) {
-              if ($tab['def'] != "") {
-                  echo $tab['def'];
-              }
-          }
-      ?>
-    </div>
-  </div>
 
+  </body>
 
-</body>
-</html>
+  </html>
--- script.sh
+++
@@ -1,1 +0,0 @@
-scp -r ./*.php mooc@webinfo.proj.info-ufr.univ-montp2.fr:/home/mooc/public_html/JDM/
